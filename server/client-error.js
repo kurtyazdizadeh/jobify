70b4515ee77cc698bcce018d417c79590e1de3dc
Mikayla Ruiz <mikaylarruiz@gamil.com>
2020-05-01 23:20:14 +0000

    added server files

---
+++ server/client-error.js
@@ -0,0 +1,9 @@
+
+class ClientError {
+  constructor(message, status) {
+    this.message = message;
+    this.status = status;
+  }
+}
+
+module.exports = ClientError;
---
+++ server/static-middleware.js
@@ -0,0 +1,8 @@
+
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, 'public/');
+const staticMiddlware = express.static(publicPath);
+
+module.exports = staticMiddlware;
