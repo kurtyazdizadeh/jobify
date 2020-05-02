f454984af982feb14c184ea2e8ea4bc3b6dfd291
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-05-02 21:55:59 +0000

    basic structure for header complete, click handlers and interaction still need to be programmed

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <nav className="navbar fixed-top bg-light">
+      <nav className="navbar fixed-top dark-green">
         <i className="fas fa-bars"></i>
         {this.props.title}
         <i className="fas fa-user-circle"></i>
