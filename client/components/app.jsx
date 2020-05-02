a74caadd3127fc0533630af29d0f1be834fdcce1
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-05-02 21:05:41 +0000

    added Header component, added Bootstrap and Font Awesome Icons to HTML

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './header';
 import SearchJobs from './search-jobs';
 
 export default class App extends React.Component {
---
+++ client/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ''
+    };
+  }
+
+  render() {
+    return <div>Header</div>;
+  }
+}
+
+export default Header;
