6d791201a0cef467bd6710222fa625985d455c9c
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-05-02 20:59:50 +0000

    created JobSearch component, tested and ensured it shows up on DOM

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchJobs from './search-jobs';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -18,8 +19,9 @@ export default class App extends React.Component {
   }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message.toUpperCase()}</h1>;
+    // return this.state.isLoading
+    //   ? <h1>Testing connections...</h1>
+    //   : <h1>{this.state.message.toUpperCase()}</h1>;
+    return <SearchJobs />;
   }
 }
---
+++ client/components/search-jobs.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SearchJobs extends React.Component {
+  render() {
+    return <div>SEARCH JOBS</div>;
+  }
+}
+
+export default SearchJobs;
