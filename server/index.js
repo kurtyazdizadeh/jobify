a213e2e263b4b44d86a71f6371bc5925aafac444
Jordan Schaefer <jschaef99@gmail.com>
2020-05-04 12:30:47 -0700

    update to rows

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ app.get('/api/specific-job/:id', (req, res, next) => {
       if (!result.rows[0]) {
         res.status(400).json({ error: `user id of ${id} not found` });
       } else {
-        res.status(200).json(result.row[0]);
+        res.status(200).json(result.rows[0]);
       }
     })
     .catch(err => next(err));
