b95ab55e95415d9598ae53ec469b32b0cf1910be
Jordan Schaefer <jschaef99@gmail.com>
2020-05-05 11:02:58 -0700

    can get note information for specific job

--- server/index.js
+++ server/index.js
@@ -59,6 +59,28 @@ app.get('/api/specific-job/:id', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/notes/:jobId', (req, res, next) => {
+  const { jobId } = req.params;
+  if (jobId <= 0) {
+    res.status(400).json({ error: 'job id must be a positive integer' });
+  }
+  const sql = `
+  SELECt "notes"."note_title",
+         "notes"."note_content",
+         "notes"."date_posted",
+         "JobNotes"."job_note_id",
+         "JobNotes"."note_id"
+    FROM "notes"
+    JOIN "JobNotes" using ("note_id")
+   WHERE "note_id" = $1
+  `;
+
+  const params = [jobId];
+
+  db.query(sql, params)
+    .then(result => res.status(200).json(result.rows[0]))
+    .catch(err => next(err));
+});
 
 app.delete('/api/saved-job/:id', (req, res, next) => {
   const { id } = req.params;
@@ -90,6 +112,7 @@ app.delete('/api/saved-job/:id', (req, res, next) => {
         error: 'An unexpected error occurred.'
       });
     });
+});
 
 app.post('/api/status/:id', (req, res, next) => {
   const { id } = req.params;
