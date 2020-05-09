420c155de069f0d96a0ea561b2bcfe9c016073a4
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-05-09 00:23:21 +0000

    uploading files to backend is done. Need to work on front end to be able to pull files from server to view if user wants to see it

--- client/components/upload-files.jsx
+++ client/components/upload-files.jsx
@@ -11,18 +11,6 @@ class UploadFiles extends React.Component {
     this.handleUploadToServer = this.handleUploadToServer.bind(this);
   }
 
-  /*
-    - Retrieve userJobId from props and use that in DB query to associate file with specific job
-    - Build 3 separate forms for each file type
-    - Create submit button for each form to prevent default behavior and send a POST request to DB
-
-    - In index.js, use multer to store the file in the public/docs directory
-    - Make sure to obscure the filename being stored in the public/docs directory with a hash
-    - Store the hashed filename in the Files table of the DB, as well as save the actual name in the table so when
-    - the user views/downloads the file, they know what they are looking at.
-
-    - figure out a way to have a modal pop up when they click "view" button to view doc in app.
-  */
   handleUploadChange() {
     event.preventDefault();
 
@@ -49,16 +37,14 @@ class UploadFiles extends React.Component {
       .then(res => res.json())
       .then(result => this.setState({ selectedFile: null, fileType: '' }))
       .catch(err => console.error(err));
-
   }
 
   render() {
-    const { selectedFile } = this.state;
+    const { selectedFile, fileType } = this.state;
     return (
       <div className='container mt-4 py-3 dark-gray'>
         <div>
           <h4 className='title align-middle text-center'>Resume</h4>
-          {/* <button className='button btn col align-items-center'>View</button> */}
           <form
             onSubmit={this.handleUploadToServer}
             encType="multipart/form-data">
@@ -71,24 +57,57 @@ class UploadFiles extends React.Component {
                 id="resume"
                 accept="application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document"
               />
-              <label className="custom-file-label" htmlFor="resume">{ selectedFile ? selectedFile.name : 'Choose File...'}</label>
+              <label className="custom-file-label" htmlFor="resume">
+                {(selectedFile && fileType === 'resume') ? selectedFile.name : 'Choose File...'}
+              </label>
             </div>
             <button type='submit' className='btn button'>Upload</button>
+            <button className='button btn col align-items-center'>View</button>
           </form>
-
-          {/* <button className='button btn col'>Delete</button> */}
         </div>
         <div>
           <h4 className='title align-middle text-center'>Cover Letter</h4>
-          <button className='button btn col'>View</button>
-          <button className='button btn col'>Upload</button>
-          <button className='button btn col'>Delete</button>
+          <form
+            onSubmit={this.handleUploadToServer}
+            encType="multipart/form-data">
+            <div className="custom-file">
+              <input
+                type="file"
+                name="cover_letter"
+                onChange={this.handleUploadChange}
+                className="custom-file-input dark-gray"
+                id="cover_letter"
+                accept="application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+              />
+              <label className="custom-file-label" htmlFor="cover_letter">
+                {(selectedFile && fileType === 'cover_letter') ? selectedFile.name : 'Choose File...'}
+              </label>
+            </div>
+            <button type='submit' className='btn button'>Upload</button>
+            <button className='button btn col'>View</button>
+          </form>
         </div>
         <div>
           <h4 className='title align-middle text-center'>Letter of Recommendation</h4>
-          <button className='button btn col'>View</button>
-          <button className='button btn col'>Upload</button>
-          <button className='button btn col'>Delete</button>
+          <form
+            onSubmit={this.handleUploadToServer}
+            encType="multipart/form-data">
+            <div className="custom-file">
+              <input
+                type="file"
+                name="letter_of_recommendation"
+                onChange={this.handleUploadChange}
+                className="custom-file-input dark-gray"
+                id="letter_of_recommendation"
+                accept="application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+              />
+              <label className="custom-file-label" htmlFor="letter_of_recommendation">
+                {(selectedFile && fileType === 'letter_of_recommendation') ? selectedFile.name : 'Choose File...'}
+              </label>
+            </div>
+            <button type='submit' className='btn button'>Upload</button>
+            <button className='button btn col'>View</button>
+          </form>
         </div>
       </div>
     );
