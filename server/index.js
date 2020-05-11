0a0d712f480f96d4b1fc4080a48374363c24b154
John Ko <jko121790@gmail.com>
2020-05-11 11:16:44 -0700

    add goals back end complete

--- server/index.js
+++ server/index.js
@@ -218,6 +218,40 @@ app.post('/api/manual-save', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.post('/api/goals/', (req, res, next) => {
+  const goal = req.body;
+  if (!parseInt(goal.endGoal, 10) || parseInt(goal.endGoal, 10) < 0) {
+    return res.status(400).json({
+      error: 'endGoal must be a positive integer'
+    });
+  }
+  if (!goal.title) {
+    return res.status(400).json({
+      error: 'title must be included'
+    });
+  }
+  if (!goal.type) {
+    return res.status(400).json({
+      error: 'type must be included'
+    });
+  }
+  const sql = `
+    INSERT INTO "UsersGoal" ("user_goal_id", "user_id", "goal_achieved", "currently_active", "current_progress", "end_goal", "goal_title", "goal_type", "deadline_date")
+            VALUES (default, 1, default, default, default, $1, $2, $3, $4)
+            RETURNING *;
+  `;
+  const params = [goal.endGoal, goal.title, goal.type, goal.deadline];
+  db.query(sql, params)
+    .then(result => {
+      if (!result.rows[0]) {
+        res.status(404).json({ error: 'something went wrong' });
+      } else {
+        res.status(201).json(result);
+      }
+    })
+    .catch(err => next(err));
+});
+
 app.post('/api/notes/', (req, res, next) => {
   const { title, content, category } = req.body;
 
