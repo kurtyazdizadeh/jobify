8c78adde515a1086523718ae53dac02b10755624
Mikayla Ruiz <mikaylarruiz@gamil.com>
2020-05-11 05:38:52 +0000

    added functionality to each set of words on the side bar to direct you to each individual page

--- client/components/side-bar.jsx
+++ client/components/side-bar.jsx
@@ -13,17 +13,17 @@ class SideBar extends React.Component {
     this.setState({ isClicked: !this.state.isClicked });
   }
 
-  render() {
+  render(props) {
     let menu;
     if (this.state.isClicked === true) {
       menu = (
         <div className='container'>
-          <div className='aside'>
-            <h6 onClick={this.toggle}>Saved Jobs</h6>
-            <h6>Search for Jobs</h6>
-            <h6>Notes</h6>
-            <h6>Activity Statistics</h6>
-            <h6>Goals</h6>
+          <div onClick={this.toggle} className='aside'>
+            <h6 onClick={() => props.setView('Saved Jobs', {})} >Saved Jobs</h6>
+            <h6 onClick={() => props.setView('Search for Jobs', {})}>Search for Jobs</h6>
+            <h6 onClick={() => props.setView('Notes', {})}>Notes</h6>
+            <h6 onClick={() => props.setView('Activity Statistics', {})}>Activity Statistics</h6>
+            <h6 onClick={() => props.setView('Goals', {})}>Goals</h6>
           </div>
         </div>
       );
