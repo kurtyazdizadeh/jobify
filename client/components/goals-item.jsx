88a92047d82360eab7e59e31e568ca34582ab409
John Ko <jko121790@gmail.com>
2020-05-12 14:46:52 -0700

    goals modifier progress buttons added

--- client/components/goals-item.jsx
+++ client/components/goals-item.jsx
@@ -1,33 +1,56 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import { render } from 'react-dom';
 
-function GoalItems(props) {
-  let backgroundColor = '';
-  let starColor = '';
-  if (props.isActive) {
-    backgroundColor = 'light-grey goalItems my-2 d-flex flex-column align-items-center';
-  } else {
-    backgroundColor = 'light-grey goalItems my-2 d-flex flex-column align-items-center';
+class GoalItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.createBackground = this.createBackground.bind(this);
+    this.createStarColor = this.createStarColor.bind(this);
   }
-  if (props.isAchieved) {
-    starColor = 'gold fas fa-star align-self-end';
-  } else {
-    starColor = 'fas fa-star align-self-end';
+
+  createBackground() {
+    let backgroundColor = '';
+
+    if (this.props.isActive) {
+      backgroundColor = 'light-grey goalItems my-2 d-flex flex-column align-items-center';
+    } else {
+      backgroundColor = 'light-grey goalItems my-2 d-flex flex-column align-items-center';
+    }
+
+    return backgroundColor;
   }
-  return (
-    <div className={backgroundColor}>
-      <i className={starColor}></i>
-      <h4 className='text-center'>{props.title}</h4>
-      <div className='w-100 p-3 d-flex justify-content-between'>
-        <button className='btn btn-secondary'>
-          <i className='fas fa-minus'></i>
-        </button>
-        <p>{props.current} / {props.end}</p>
-        <button className='btn btn-secondary'>
-          <i className='fas fa-plus'></i>
-        </button>
+
+  createStarColor() {
+    let starColor = '';
+    if (this.props.isAchieved) {
+      starColor = 'gold fas fa-star align-self-end';
+    } else {
+      starColor = 'fas fa-star align-self-end';
+    }
+    return starColor;
+  }
+
+  render() {
+    const backgroundColor = this.createBackground();
+    const starColor = this.createStarColor();
+    return (
+      <div className={backgroundColor}>
+        <i className={starColor}></i>
+        <h4 className='text-center'>{this.props.title}</h4>
+        <div className='w-100 p-3 d-flex justify-content-between'>
+          <button className='btn btn-secondary'>
+            <i className='fas fa-minus'></i>
+          </button>
+          <p>{this.props.current} / {this.props.end}</p>
+          <button className='btn btn-secondary'>
+            <i className='fas fa-plus'></i>
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default GoalItems;
