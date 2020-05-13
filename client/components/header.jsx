397137fffc14c23a5007fd67df12ed98bacbeb59
Mikayla Ruiz <mikaylarruiz@gamil.com>
2020-05-13 00:24:08 +0000

    added links to sidebar

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   constructor(props) {
@@ -17,18 +18,26 @@ class Header extends React.Component {
     let menu;
     if (this.state.isClicked === true) {
       menu = (
-        <div className='container '>
-          <nav className="navbar fixed-top dark-green ">
+        <div className='shaded'>
+          <nav className="navbar fixed-top dark-green">
             <i onClick={this.handleClick} className="fas fa-bars pointer"></i>
             {this.props.title}
             <i onClick={() => this.props.setView('Profile', {})} className="fas fa-user-circle pointer"></i>
           </nav>
           <div onClick={this.handleClick} className="side-menu">
-            <h6 onClick={() => props.setView('Saved Jobs', {})} >Saved Jobs</h6>
-            <h6 onClick={() => props.setView('Search for Jobs', {})}>Search for Jobs</h6>
-            <h6 onClick={() => props.setView('Notes', {})}>Notes</h6>
-            <h6 onClick={() => props.setView('Activity Statistics', {})}>Activity Statistics</h6>
-            <h6 onClick={() => props.setView('Goals', {})}>Goals</h6>
+            <Link to="/home">
+              <h6 onClick={() => this.props.setView('Home')} >Saved Jobs</h6>
+            </Link>
+            <Link to="/search">
+              <h6 onClick={() => this.props.setView('Search', {})}>Search Jobs</h6>
+            </Link>
+            <Link to="/notes">
+              <h6 onClick={() => this.props.setView('Notes', {})}>Notes</h6>
+            </Link>
+            <Link to="/goals">
+              <h6 onClick={() => this.props.setView('Goals', {})}>Goals</h6>
+            </Link>
+
           </div>
         </div>
       );
