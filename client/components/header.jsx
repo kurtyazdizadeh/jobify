9d70e3e976887f6cf4743e14b80d8557685190c1
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-05-13 23:11:24 +0000

    wiped out database to start with new data

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -15,46 +15,46 @@ class Header extends React.Component {
   }
 
   render(props) {
-    let menu;
-    if (this.state.isClicked === true) {
-      menu = (
-        <div className='shaded'>
+    if (this.state.isClicked) {
+      return (
+        <>
           <nav className="navbar fixed-top dark-green">
             <i onClick={this.handleClick} className="fas fa-bars pointer"></i>
             {this.props.title}
-            <i onClick={() => this.props.setView('Profile', {})} className="fas fa-user-circle pointer"></i>
+            <i onClick={() => this.props.setView('Profile')} className="fas fa-user-circle pointer"></i>
           </nav>
-          <div onClick={this.handleClick} className="side-menu">
-            <Link to="/home">
-              <h6 onClick={() => this.props.setView('Home')} >Saved Jobs</h6>
-            </Link>
-            <Link to="/search">
-              <h6 onClick={() => this.props.setView('Search', {})}>Search Jobs</h6>
-            </Link>
-            <Link to="/notes">
-              <h6 onClick={() => this.props.setView('Notes', {})}>Notes</h6>
-            </Link>
-            <Link to="/goals">
-              <h6 onClick={() => this.props.setView('Goals', {})}>Goals</h6>
-            </Link>
-
+          <div className='shaded'>
+            <div onClick={this.handleClick} className="side-menu">
+              <Link to="/home">
+                <h6 onClick={() => this.props.setView('Home')} >Saved Jobs</h6>
+              </Link>
+              <Link to="/search">
+                <h6 onClick={() => this.props.setView('Search')}>Search Jobs</h6>
+              </Link>
+              <Link to="/notes">
+                <h6 onClick={() => this.props.setView('Notes')}>Notes</h6>
+              </Link>
+              <Link to="/goals">
+                <h6 onClick={() => this.props.setView('Goals')}>Goals</h6>
+              </Link>
+              <Link to="/map">
+                <h6 onClick={() => this.props.setView('Map')}>Map</h6>
+              </Link>
+            </div>
           </div>
-        </div>
+        </>
       );
-    } else if (this.state.isClicked === false) {
-      menu = (
-        <div>
+    } else {
+      return (
+        <>
           <nav className="navbar fixed-top dark-green">
             <i onClick={this.handleClick} className="fas fa-bars pointer"></i>
             {this.props.title}
-            <i onClick={() => this.props.setView('Profile', {})} className="fas fa-user-circle pointer"></i>
+            <i onClick={() => this.props.setView('Profile')} className="fas fa-user-circle pointer"></i>
           </nav>
-          <div >
-          </div>
-        </div>
+        </>
       );
     }
-    return menu;
   }
 }
 
